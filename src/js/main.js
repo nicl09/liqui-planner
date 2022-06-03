cc7697780bea6aaabb72b4ff077f047b615a4e88
nicl09 <niclas.behrendt@nicl.dev>
2022-06-04 00:05:22 +0200

    Finished the first prompts and variables

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,22 @@
 "use strict";
 
-console.log('Hello World');
+// [x] Add variable for title, type, amount, date
+// [x] Use prompts and save to variable
+// [x] Log to console
+
+
+// Get inputs
+let title = prompt('Please type a title.');
+let type = prompt('Please set a type.');
+let amount = prompt('Please set the amount.');
+let date = prompt('Please set the correct date.');
+
+console.log('Title: ' + title);
+console.log('Type: ' + type);
+console.log('Amount: ' + amount);
+console.log('Date: ' + date);
+
+// Overall balance
+let income;
+let outgoing;
+let balance;
